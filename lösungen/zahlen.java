09236a3ad7c65da7902e82a2587f6b8cca0198be
Enno Muensterberg <enno.muensterberg@xqand.com>
2015-10-17 13:41:30 +0200

    uebung 1 ohne den zweiten teil

---
+++ lösungen/zahlen.java
@@ -0,0 +1,27 @@
+package mypackage;
+
+public class zahlen {
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4,5,8,3,9,7};
+		int min = 10,
+			max = 0,
+			sum = 0,
+			ds = 0,
+			n = 9;
+		
+		for(int i = 0; i < n; i++){
+			sum = sum + a[i];
+			if(min > a[i]){
+				min = a[i];
+			}
+			
+			if(max < a[i])
+				max = a[i];
+		}
+		
+		ds = sum/n;
+		System.out.println("Minimum: " + min + " Maximum: " + max + " Summe: " + sum + " Duchschnitt: " + ds);
+		
+	}
+
+}
