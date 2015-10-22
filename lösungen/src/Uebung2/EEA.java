2d54929c3f717cd3fee3b67826d19982a4e8c4f2
Enno Muensterberg <enno.muensterberg@xqand.com>
2015-10-22 14:25:39 +0200

    hinzufügen von neuen packeten und dateien

---
+++ lösungen/src/Uebung2/EEA.java
@@ -0,0 +1,10 @@
+package Uebung2;
+
+public class EEA {
+	public static void main(String[] args) {
+		int a = 0,
+			b = 1,
+			alpha = 0,
+			beta = 1;
+	}
+}
---
+++ lösungen/src/Uebung2/demorgan.java
@@ -0,0 +1,27 @@
+package Uebung2;
+
+public class demorgan{
+	public static void main(String[] args) {
+		boolean a = true,
+				b = true;
+		
+		for(int i = 0; i < 4; i++) {
+			if(!(a && b) == !a || !b) {
+				System.out.println("Erste Regel bewiesen!");
+				System.out.println("Da: " + (!(a && b)) + " == " + (!a || !b));
+			}
+			
+			if(!(a || b) == (!a && !b)) {
+				System.out.println("Zweite Regel bewiesen!");
+				System.out.println("Da: " + (!(a || b)) + " == " + (!a && !b));
+			}
+			if(a == true) {
+				a = false;
+			} else if(b == false) {
+				b = true;
+			} else {
+				b = false;
+			}
+		}
+	}
+}
