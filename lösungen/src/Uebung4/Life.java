00ad1e2967403ce30786d8699ed51f2f084528a3
root <root@BOOS>
2015-10-23 08:39:44 +0200

    kleine schönheits anpassungen

--- lösungen/src/Uebung4/Life.java
+++ lösungen/src/Uebung4/Life.java
@@ -2,11 +2,14 @@ public class Life {
 	public static void main(String[] args) {
 		int spalten = 23,
 			zeilen = 13,
+			i = 0,
+			j = 0,
 			m = 2;
+
 		boolean[][] arr = new boolean[zeilen][spalten];
 		
-		for(int i = 0; i < zeilen; i++) {
-			for(int j = 0; j < spalten; j++) {
+		for(i = 0; i < arr.length; ++i) {
+			for(j = 0; j < arr[i].length; ++j) {
 				if(j % m == 0) {
 					arr[i][j] = true;
 					System.out.print('X');
